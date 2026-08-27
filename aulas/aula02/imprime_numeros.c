e63118bda12e1b433f37ad82a3414e39284c97ce
Ana Clara de Oliveira dos Santos <oanaclara980@gmail.com>
2026-08-26 21:56:18 -0300

    codigo da aula2

--- aulas/aula02/imprime_numeros.c
+++ aulas/aula02/imprime_numeros.c
@@ -9,5 +9,23 @@ printf("10+5= %i\n",10+5);
 
 printf("%f", 8.5);
 printf("%f",8.5);
+printf(" CPF %lli=\n",11111111111);
+// preenche com espaco ate 11 digatos
+printf("CPF =%11i\n",00000000001);
+// preenche com o 0 ate 11 digitos
+printf("CPF =%011i\n",00000000001);
+
+printf("Preco = r$ %7.2f\n",100.0);
+printf("Preco = R$ %7.2f\n",10.0);
+printf("Preco = R$ %7.2f\n",1000.0);
+printf("CPF =%011i\n",00000000001);
+
+
+
+
+
+
+
 return 0;
+
 }
--- aulas/aula02/imprime_texto.c
+++ aulas/aula02/imprime_texto.c
@@ -5,6 +5,25 @@ int main () {
 //funcao para escrever algo na tela 
 printf("C é muito legal!\n\n");
 printf("Vamos codar");
+
+//"" eh texto (string) '' eh caracter
+printf("A terceira letra do alfabeto eh %c\n e a ultima eh %c\n", 'c', 'z');
+printf("Ola,%s!\n","Ana");
+
+printf("========================\n");
+printf("    NOTA LEGAL   \n");
+printf("Item     Unid   Preco Valor\n");
+printf("Caneta      1   1,00  1,00\n");
+printf("Borracha    1   5,00  5,00\n");
+printf("Lapis       2   2,50  5,00\n");
+printf("-----------------------------\n");
+printf("TOTAL:11,00\n");
+printf("---------------------------\n");
+
+
+
+
 return 0;
+
 
 }
